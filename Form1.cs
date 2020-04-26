d96dfccee73936a0ac9745abfd048698b0416a67
ydaglar <yucel.daglar@smartface.io>
2020-04-26 14:30:43 +0300

    first commit

---
+++ Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MyTetris
+{
+    public partial class Form1 : Form
+    {
+        TetrisGame Game = new TetrisGame();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Timer1_Tick(object sender, EventArgs e)
+        {
+            Game.GameLoop();
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            if (Game.Matrix == null) return;
+            int c = 20;
+            int w = Game.Matrix.GetUpperBound(0);
+            int h = Game.Matrix.GetUpperBound(1);
+            int offsetX = 20;
+            int offsetY = 50-80;
+            e.Graphics.DrawRectangle(Pens.Black, offsetX + 0, offsetY + 80, c * (w + 1), c * (h + 1 - 4));
+            for (int x = 0; x <= w; x++)
+            {
+                for (int y = 4; y <= h; y++)
+                {
+                    if (Game.Matrix[x, y] > 0)
+                    {
+                        e.Graphics.FillRectangle(Brushes.Green, offsetX + x * c, offsetY + y * c, c, c);
+                        e.Graphics.DrawRectangle(Pens.Black, offsetX + x * c, offsetY + y * c, c, c);
+                    }
+                    e.Graphics.DrawRectangle(Pens.Black, offsetX + x * c, offsetY + y * c, c, c);
+                }
+            }
+            for (int x = 0; x <= Game.CurrentPiece?.GetUpperBound(0); x++)
+            {
+                for (int y = 0; y <= Game.CurrentPiece?.GetUpperBound(1); y++)
+                {
+                    if (Game.CurrentPiece[x, y] == 0 || (y + Game.CurrentY) < 4) continue;
+                    e.Graphics.FillRectangle(Brushes.Green, offsetX + (x + Game.CurrentX) * c, offsetY + (y + Game.CurrentY) * c, c, c);
+                    e.Graphics.DrawRectangle(Pens.Black, offsetX + (x + Game.CurrentX) * c, offsetY + (y + Game.CurrentY) * c, c, c);
+                }
+            }
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            Game.StartNewGame();
+        }
+
+        protected override bool ProcessCmdKey(ref Message msg, Keys keyData)
+        {
+            if (keyData == Keys.Left)
+                Game.MoveLeft();
+            else if (keyData == Keys.Right)
+                Game.MoveRight();
+            else if (keyData == Keys.Up)
+                Game.TurnPiece();
+            else if (keyData == Keys.Down)
+                Game.MoveDown();
+
+            return base.ProcessCmdKey(ref msg, keyData);
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            this.Refresh();
+        }
+    }
+}
---
+++ TestrisGame.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyTetris
+{
+    class TetrisGame
+    {
+        public bool IsPlaying { get; set; }
+        public int[,] Matrix;
+        public int CurrentX;
+        public int CurrentY;
+        public int[,] CurrentPiece;
+
+        public void GameLoop()
+        {
+            if (!IsPlaying) return;
+
+            if (!MoveDown())
+            {
+                PlacePiece(Matrix, CurrentX, CurrentY, CurrentPiece);
+                DeleteFullLines(Matrix);
+                GenerateNewPiece();
+            }
+        }
+
+        public void StartNewGame()
+        {
+            Matrix = new int[12, 20];
+            IsPlaying = true;
+
+            GenerateNewPiece();
+        }
+
+        public void MoveLeft()
+        {
+            if (!IsMatrixEmpty(CurrentX - 1, CurrentY, CurrentPiece, Matrix)) return;
+            CurrentX--;
+        }
+
+        public void MoveRight()
+        {
+            if (!IsMatrixEmpty(CurrentX + 1, CurrentY, CurrentPiece, Matrix)) return;
+            CurrentX++;
+        }
+
+        public void TurnPiece()
+        {
+            if (CurrentPiece == null) return;
+
+            var len = CurrentPiece.GetUpperBound(0) + 1;
+            var newPiece = new int[len, len];
+            MatrixEach(CurrentPiece, (x, y) => newPiece[len - y -1, x] = CurrentPiece[x, y]);
+            if (!IsMatrixEmpty(CurrentX, CurrentY, newPiece, Matrix)) return;
+
+            CurrentPiece = newPiece;
+        }
+
+        public bool MoveDown()
+        {
+            if (!IsMatrixEmpty(CurrentX, CurrentY + 1, CurrentPiece, Matrix)) return false;
+
+            CurrentY++;
+            return true;
+        }
+
+        private void GenerateNewPiece()
+        {
+            int pieceIndex = new Random().Next(7);
+            CurrentX = 3;
+            CurrentY = 0;
+            CurrentPiece = GetPieceArray(pieceIndex);
+            if (!IsMatrixEmpty(CurrentX, CurrentY, CurrentPiece, Matrix)) IsPlaying = false;
+        }
+
+
+        private static void MatrixEach(int[,] matrix, Action<int, int> action)
+        {
+            for (int x = 0; x <= matrix.GetUpperBound(0); x++)
+                for (int y = 0; y <= matrix.GetUpperBound(1); y++) action(x, y);
+        }
+        
+        private static void PlacePiece(int[,] matrix, int x, int y, int[,] piece)
+        {
+            for (int u = 0; u <= piece.GetUpperBound(0); u++)
+            {
+                for (int v = 0; v <= piece.GetUpperBound(1); v++)
+                {
+                    if (piece[u, v] == 0) continue;
+
+                    matrix[x + u, y + v] = piece[u, v];
+                }
+            }
+        }
+
+        private static bool IsMatrixEmpty(int x, int y, int[,] piece, int[,] matrix)
+        {
+            for (int u = 0; u <= piece.GetUpperBound(0); u++)
+            {
+                for (int v = 0; v <= piece.GetUpperBound(1); v++)
+                {
+                    if (piece[u, v] == 0) continue;
+
+                    var a = x + u;
+                    var b = y + v;
+
+                    if ( a < 0
+                        || a > matrix.GetUpperBound(0)
+                        || b > matrix.GetUpperBound(1)) return false;
+
+                    if (matrix[a, b] > 0) return false;
+                }
+            }
+
+            return true;
+        }
+
+        private static void DeleteFullLines(int[,] matrix)
+        {
+            int lineIndex = matrix.GetUpperBound(1);
+            for (int i = 0; i <= matrix.GetUpperBound(1); i++)
+            {
+                if (LineIsFull(matrix, lineIndex))
+                {
+                    DeleteLine(matrix, lineIndex);
+                    continue;
+                }
+                lineIndex--;
+            }
+        }
+
+        private static bool LineIsFull(int[,] matrix, int yCoord)
+        {
+            for (int x = 0; x <= matrix.GetUpperBound(0); x++)
+            {
+                if (matrix[x, yCoord] == 0) return false;
+            }
+
+            return true;
+        }
+
+        private static void DeleteLine(int[,] matrix, int yCoord)
+        {
+            var xLength = matrix.GetUpperBound(0) + 1;
+            for (int y = yCoord; y > 1; y--)
+            {
+                for (int x = 0; x < xLength; x++)
+                {
+                    matrix[x, y] = matrix[x, y - 1];
+                }
+            }
+            for (int x = 0; x < xLength; x++)
+            {
+                matrix[x, 0] = 0;
+            }
+        }
+
+        private static int[,] GetPieceArray(int pieceIndex)
+        {
+            switch (pieceIndex)
+            {
+                case 0: return new int[4, 4] { { 0, 0, 0, 0 }, { 1, 1, 1, 1 }, { 0, 0, 0, 0 }, { 0, 0, 0, 0 } };
+                case 1: return new int[4, 4] { { 0, 0, 0, 0 }, { 1, 1, 1, 0 }, { 0, 0, 1, 0 }, { 0, 0, 0, 0 } };
+                case 2: return new int[4, 4] { { 0, 0, 0, 0 }, { 0, 0, 1, 0 }, { 1, 1, 1, 0 }, { 0, 0, 0, 0 } };
+                case 3: return new int[4, 4] { { 0, 1, 0, 0 }, { 0, 1, 1, 0 }, { 0, 0, 1, 0 }, { 0, 0, 0, 0 } };
+                case 4: return new int[4, 4] { { 0, 0, 1, 0 }, { 0, 1, 1, 0 }, { 0, 1, 0, 0 }, { 0, 0, 0, 0 } };
+                case 5: return new int[4, 4] { { 1, 1, 1, 0 }, { 1, 1, 1, 0 }, { 1, 1, 1, 0 }, { 0, 0, 0, 0 } };
+                case 6: return new int[4, 4] { { 0, 1, 0, 0 }, { 0, 1, 1, 0 }, { 0, 1, 0, 0 }, { 0, 0, 0, 0 } };
+            }
+            throw new Exception("invalid piece index");
+        }
+    }
+}
